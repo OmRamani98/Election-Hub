40a420e049c587cb32892287c315d014bbb2fd9a
OM RAMANI <omramani98@gmail.com>
2024-02-17 16:04:18 +0530

    VoteRepository Created

---
+++ backend/src/main/java/com/example/electionhub/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package com.example.electionhub.repository;
+import com.example.electionhub.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+}
